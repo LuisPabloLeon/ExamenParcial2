fbd03de4af949c96aaf3c04ae63faa5c2abacad3
LuisPabloLeon <163448689+LuisPabloLeon@users.noreply.github.com>
2024-05-08 21:20:04 -0600

    Add files via upload

---
+++ Proyecto_P2-main/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+    //Luis Pablo Leon Capitan DI22114
+    //ExamenParcial2 V1.0
+    //Fecha 07/05/24
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void guardar_Click(object sender, EventArgs e)
+        {
+            //condicional que comprueba que las cajas de texto contangan escrito algo
+            if (textBox1.Text == "" || textBox2.Text == "" || textBox3.Text == "" || textBox4.Text == "" || textBox5.Text == "" || textBox6.Text == "")
+            {
+                MessageBox.Show("No ha terminado de llenar la informacion");
+            }
+            else
+            {
+                MessageBox.Show("Tus datos han sido guardados :D");
+            }// esto se hace de tarea todos los valores
+            // deben de almacenarse en su
+            // caja de memoria correspondiente
+            // usando la clase hija que corresponda
+        }
+
+        private void limpiar_Click(object sender, EventArgs e)
+        {
+            //Limpieza de las cajas de texto
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox5.Clear();
+            textBox6.Clear();// se hace de tarea
+            // debe de borrar las cajas de texto
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+            //Cerrar programa
+            Application.Exit();// se hace de tarea
+            // debe de terminar la ejecucion de la interfaz
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void validar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Proyecto_P2-main/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace persona_herencia
+{
+    class Persona //clase padre
+    {
+        protected string NombreCompleto;
+        protected string FechaN;
+        protected int Edad;
+        protected int DNI_Matricula;
+        protected string Carrera_Puesto;
+
+        public string nombrecompleto //Caja de memoria
+        {
+            get { return NombreCompleto; }
+            set { NombreCompleto = value; }
+        }
+        public string fechan //caja de memoria
+        {
+            get { return FechaN; }
+            set { FechaN = value; }
+        }
+        public int edad //caja de memoria
+        {
+            get { return Edad; }
+            set { Edad = value; }
+        }
+        public int dni_matricula //caja de memoria
+        {
+            get { return DNI_Matricula; }
+            set { DNI_Matricula = value; }
+        }
+        public string carrera_Puesto //Caja de memoria
+        {
+            get { return Carrera_Puesto; }
+            set { Carrera_Puesto = value; }
+        }
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+        //aqui va el constructor para cargar datos en las cajas de memoria
+        //nombre, edad, fecha de nacimiento(es un tipo cadena!!!!! string)
+    }
+
+    class Alumno :Persona //clase hija alumno
+    {
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+    }
+    class Empleado : Persona //clase hija
+    {
+        protected int Sueldo;
+        public int sueldo//caja de memoria
+        {
+            get { return Sueldo; }
+            set { Sueldo = value; }
+        }
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+    }
+
+    class Docente : Persona //clase Hija
+    {
+        protected int Sueldo;
+        public int sueldo//caja de memoria
+        {
+            get { return Sueldo; }
+            set { Sueldo = value; }
+        }
+        //variables
+        //atributos
+        //metodos
+        //colocar todos los comentarios necesarios para exlicar
+        //el funcinamiento de su código
+    }
+    static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
